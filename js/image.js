9518483c7bc5f01140c4204cf5922976002eff01
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-09-19 11:41:28 +0530

    Added custom lazy loading to images

---
+++ js/image.js
@@ -0,0 +1,22 @@
+const Lazy_LOADER_CLASS = "lazy-load"
+const LOADER_SPINNER = "swiper-lazy-preloader"
+
+let imgObserver = new IntersectionObserver((entries,observer)=>{
+    // entries is an array
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            console.log(entry.target)
+            entry.target.classList.remove(Lazy_LOADER_CLASS)
+            entry.target.classList.remove(LOADER_SPINNER)
+            entry.target.src = entry.target.getAttribute("data-src")
+            observer.unobserve(entry.target)
+        }
+    })
+},{
+    rootMargin:"50%"
+})
+
+let images = document.querySelectorAll(`.${Lazy_LOADER_CLASS}`)
+images.forEach(img=>{
+    imgObserver.observe(img)
+})
